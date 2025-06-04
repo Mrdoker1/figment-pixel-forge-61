73948d8c477cc4f3a301c2b58c8c1c77278d6a6d
Viachas Kul <fixrapdoK@gmail.com>
2025-06-04 13:20:48 +0300

    Remove unused files and components for static HTML version of feedback form

--- src/components/feedback/index.ts
+++
@@ -1,6 +0,0 @@
-export { FeedbackForm } from './FeedbackForm';
-export { FeedbackHeader } from './FeedbackHeader';
-export { RatingQuestion } from './RatingQuestion';
-export { RatingSlider } from './RatingSlider';
-export { CommentInput } from './CommentInput';
-export { SubmitButton } from './SubmitButton';
